06b97189a129bd369473d7a36e1f51111c0790d2
Ricardo <20230332@ricaldone.edu.sv>
2024-09-06 15:37:51 -0600

    Cambios en el crud del menu

--- Proyecto_PTC/src/Controlador/Ctrl_Menus_PTC.java
+++ Proyecto_PTC/src/Controlador/Ctrl_Menus_PTC.java
@@ -2,11 +2,11 @@ package Controlador;
 
 import Modelo.Menus_PTC;
 import Vista.frmMenus_PTC;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
-public class Ctrl_Menus_PTC implements MouseListener{
+public class Ctrl_Menus_PTC implements ActionListener{
 
     private frmMenus_PTC vista;
     private Menus_PTC modelo;
@@ -15,33 +15,42 @@ public class Ctrl_Menus_PTC implements MouseListener{
         this.vista = Vista;
         this.modelo = Modelo;
         
-        vista.btnGuardar.addMouseListener(this); 
+        vista.btnGuardar.addActionListener(this); 
         
-        vista.jtbMenu.addMouseListener(this);
         modelo.Mostrar(vista.jtbMenu);
-        vista.btnEliminar.addMouseListener(this);
-        vista.btnActualizar.addMouseListener(this);
-        vista.jtbMenu.addMouseListener(this);
+        vista.btnEliminar.addActionListener(this);
+        vista.btnActualizar.addActionListener(this);
+        vista.btnLimpiar.addActionListener(this);
+        vista.jtbMenu.addMouseListener(new java.awt.event.MouseAdapter() {
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                modelo.cargarDatosTabla(vista);
+            }
+        });
+
+        modelo.Mostrar(vista.jtbMenu);
     }
-        
-        
+    
+       private void limpiarCampos() {
+        vista.txtCategoria.setText("");
+        vista.txtImagen.setText("");
+    }
+       
     @Override
-    public void mouseClicked(MouseEvent e) {
-      if(e.getSource() == vista.btnGuardar){
+    public void actionPerformed(ActionEvent e) {
+       if(e.getSource() == vista.btnGuardar){
           modelo.setcategoria(vista.txtCategoria.getText());
           modelo.setimagen_categoria(vista.txtImagen.getText());
           
           modelo.Guardar();
           modelo.Mostrar(vista.jtbMenu);
+          limpiarCampos();
+
       }    
       
       if(e.getSource() == vista.btnEliminar){
       modelo.Eliminar(vista.jtbMenu);
       modelo.Mostrar(vista.jtbMenu);
-      }
-      
-      if(e.getSource() == vista.jtbMenu){
-          modelo.cargarDatosTabla(vista);
       }
       
       if(e.getSource() == vista.btnActualizar){
@@ -51,23 +60,13 @@ public class Ctrl_Menus_PTC implements MouseListener{
           modelo.Actualizar(vista.jtbMenu);
           modelo.Mostrar(vista.jtbMenu);
       }
-    }
-
     
-    @Override
-    public void mousePressed(MouseEvent e) {
-    }
+        if (e.getSource() == vista.btnLimpiar) {
+            limpiarCampos(); 
+        }
 
-    @Override
-    public void mouseReleased(MouseEvent e) {
+        if (e.getSource() == vista.btnRegresarMenu) {
+            vista.dispose(); 
+        }
     }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-    }
-    
 }
